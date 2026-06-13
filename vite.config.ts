3d74a7984b9105b091271ca410fd5ebd95ee4fa0
Joost Duisters <j.h.duisters@gmail.com>
2026-06-13 15:28:52 +0100

    Sort out wrangler build for cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Served from https://<user>.github.io/worldcup-visualizer/ on GitHub Pages.
-  base: '/worldcup-visualizer/',
   plugins: [react()],
 })
